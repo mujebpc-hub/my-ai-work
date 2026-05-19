4984bd53facd95c69e71fe567462ff9db25c2c1a
mujebpc-hub <mujebpc@gmail.com>
2026-05-19 15:18:20 +0530

    Change API endpoint for model inference

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ generateBtn.addEventListener("click", async()=>{
 
         const response = await fetch(
 
-        "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
+        "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-schnell",
 
         {
 
